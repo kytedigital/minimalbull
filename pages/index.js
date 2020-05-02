60ffd3cc50c3692b4d08d8e2c78440f77f25537b
Kyle <kyle@arkade.com.au>
2020-05-02 22:00:04 +1000

    Project startup

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Minimal Bull</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Minimal Bull
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Journey of a minimalist investor.
         </p>
 
         <div className="grid">
